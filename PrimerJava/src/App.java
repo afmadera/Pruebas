cfdf2293269c340fffdb13e05017e1e10f70cc71
Andres Madera <andresmaderagarcia@gmail.com>
2024-01-30 21:13:41 -0500

    Hola mundanos

---
+++ PrimerJava/src/App.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args)  {
+        ArrayList<Object> miarray = new ArrayList<>();
+
+        miarray.add("Hola");
+        Map<Object, Object> miDic = new HashMap<>();
+
+        miDic.put("Saludo", "Buen dia" + 3);
+        miarray.add(miDic);
+
+        miarray.add("Buenos dias");
+        miDic.put(3,6);
+
+        for (Object dato: miarray){
+            System.out.println(dato);
+        }
+    }
+}
