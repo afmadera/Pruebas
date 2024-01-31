0c04adf5371293b3a24bd128b9ea72019b801292
Andres Madera <andresmaderagarcia@gmail.com>
2024-01-30 22:20:46 -0500

    Mi commit con mi nueva rama

--- PrimerJava/src/App.java
+++ PrimerJava/src/App.java
@@ -26,7 +26,7 @@ public class App {
 
     public static void contar(int indice)  {
         for (int i = 0; i <= indice; i++){
-            System.out.println(i+1);
+            System.out.println("Numero: "+(i+1));
         }
     }
 }
